1bad68b8c18d379f365fc9b31fc30b350f9300c4
Ceyhun Abbasov <ceyhun.abbasov@aist.group>
2022-05-13 11:38:37 +0400

    Add DictionaryDuplicateKey exception class

--- ConsoleTest/ConsoleTest/Data Stuctures/CustomDictionary.cs
+++ ConsoleTest/ConsoleTest/Data Stuctures/CustomDictionary.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleTest.Exceptions;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,7 +29,7 @@ namespace ConsoleTest
             var keyPresent = _values[hash].Any(p => p.Key.Equals(key));
             if (keyPresent)
             {
-                throw new Exception("Duplicate key has been found");
+                throw new DictionaryDuplicateKey("Duplicate key has been found");
             }
 
             var newValue = new KeyValuePair<TKey, UValue>(key, value);
---
+++ ConsoleTest/ConsoleTest/Exceptions/DictionaryDuplicateKey.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleTest.Exceptions
+{
+    class DictionaryDuplicateKey : Exception
+    {
+        private string message;
+        private Exception innerException;
+        public DictionaryDuplicateKey(string message)
+                :base()
+        { 
+        }
+
+        public DictionaryDuplicateKey(string message,Exception innerException)
+                :base(message, innerException)
+        {
+            this.message = message;
+            this.innerException = innerException;
+        }
+    }
+}
