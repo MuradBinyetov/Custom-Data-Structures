b2bfe2eb115f3c1b9a005011fb66a6ce0477ce19
MuradBinyetov <murad.binyatov@aist.group>
2022-06-01 10:24:12 +0400

    Add ValidParantheses algorithm and Queue data structure.

--- ConsoleTest/ConsoleTest/Program.cs
+++ ConsoleTest/ConsoleTest/Program.cs
@@ -14,14 +14,8 @@ namespace ConsoleTest
     class Program
     { 
         static void Main(string[] args)
-        { 
-            List<int> list = new List<int>();   
-            CustomList<int> lt = new CustomList<int>();
-            lt.Add(2);
-            lt.Add(3); 
-            lt.Add(4);   
-            lt.Add(5);   
-            lt.Add(6);  
+        {
+            ValidParantheses.IsValid("{[()]}");
         }  
     }
 }
